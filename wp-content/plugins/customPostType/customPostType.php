d01d1e76fd4a41721f34f3ca65ae9b1fef384585
asadise <asadicse@yahoo.com>
2018-09-27 15:07:34 +0330

    add taxonimies to customPostType plugin

--- wp-content/plugins/customPostType/customPostType.php
+++ wp-content/plugins/customPostType/customPostType.php
@@ -1,9 +1,125 @@
 <?php
 /*
-Plugin Name:  Customize PostType
-Description: Site-Specific Plugin for Create a Custom Post Typ
+Plugin Name:  Customize PostType & Taxonomy
+Description: Site-Specific Plugin for Create a Custom Post Type & Taxonomies
 */
 
+function create_genre_nonhierarchical_taxonomy() {
+  $labels = array(
+    'name' => _x( 'Genre', 'taxonomy general name' ),
+    'singular_name' => _x( 'Genre', 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search Genre' ),
+    'popular_items' => __( 'Popular Genre' ),
+    'all_items' => __( 'All Genre' ),
+    'parent_item' => null,
+    'parent_item_colon' => null,
+    'edit_item' => __( 'Edit Genre' ),
+    'update_item' => __( 'Update Genre' ),
+    'add_new_item' => __( 'Add New Genre' ),
+    'new_item_name' => __( 'New Genre Name' ),
+    'separate_items_with_commas' => __( 'Separate Genre with commas' ),
+    'add_or_remove_items' => __( 'Add or remove Genre' ),
+    'choose_from_most_used' => __( 'Choose from the most used Genre' ),
+    'menu_name' => __( 'Genre' ),
+  );
+  register_taxonomy('Genre','Films',array(
+    'hierarchical' => false,
+    'labels' => $labels,
+    'show_ui' => true,
+    'show_admin_column' => true,
+    'update_count_callback' => '_update_post_term_count',
+    'query_var' => true,
+    'rewrite' => array( 'slug' => 'genre' ),
+  ));
+}
+function create_Country_nonhierarchical_taxonomy() {
+    $labels = array(
+      'name' => _x( 'Country', 'taxonomy general name' ),
+      'singular_name' => _x( 'Country', 'taxonomy singular name' ),
+      'search_items' =>  __( 'Search Country' ),
+      'popular_items' => __( 'Popular Country' ),
+      'all_items' => __( 'All Country' ),
+      'parent_item' => null,
+      'parent_item_colon' => null,
+      'edit_item' => __( 'Edit Country' ),
+      'update_item' => __( 'Update Country' ),
+      'add_new_item' => __( 'Add New Country' ),
+      'new_item_name' => __( 'New Country Name' ),
+      'separate_items_with_commas' => __( 'Separate Countries with commas' ),
+      'add_or_remove_items' => __( 'Add or remove Country' ),
+      'choose_from_most_used' => __( 'Choose from the most used Country' ),
+      'menu_name' => __( 'Country' ),
+    );
+    register_taxonomy('Country','Films',array(
+      'hierarchical' => false,
+      'labels' => $labels,
+      'show_ui' => true,
+      'show_admin_column' => true,
+      'update_count_callback' => '_update_post_term_count',
+      'query_var' => true,
+      'rewrite' => array( 'slug' => 'country' ),
+    ));
+  }
+  function create_Year_nonhierarchical_taxonomy() {
+    $labels = array(
+      'name' => _x( 'Year', 'taxonomy general name' ),
+      'singular_name' => _x( 'Year', 'taxonomy singular name' ),
+      'search_items' =>  __( 'Search Year' ),
+      'popular_items' => __( 'Popular Year' ),
+      'all_items' => __( 'All Year' ),
+      'parent_item' => null,
+      'parent_item_colon' => null,
+      'edit_item' => __( 'Edit Year' ),
+      'update_item' => __( 'Update Year' ),
+      'add_new_item' => __( 'Add New Year' ),
+      'new_item_name' => __( 'New Year Name' ),
+      'separate_items_with_commas' => __( 'Separate Countries with commas' ),
+      'add_or_remove_items' => __( 'Add or remove Year' ),
+      'choose_from_most_used' => __( 'Choose from the most used Year' ),
+      'menu_name' => __( 'Year' ),
+    );
+
+
+    register_taxonomy('Year','Films',array(
+      'hierarchical' => false,
+      'labels' => $labels,
+      'show_ui' => true,
+      'show_admin_column' => true,
+      'update_count_callback' => '_update_post_term_count',
+      'query_var' => true,
+      'rewrite' => array( 'slug' => 'year' ),
+    ));
+  }
+  function create_Actor_nonhierarchical_taxonomy() {
+    $labels = array(
+      'name' => _x( 'Actor', 'taxonomy general name' ),
+      'singular_name' => _x( 'Actor', 'taxonomy singular name' ),
+      'search_items' =>  __( 'Search Actor' ),
+      'popular_items' => __( 'Popular Actor' ),
+      'all_items' => __( 'All Actor' ),
+      'parent_item' => null,
+      'parent_item_colon' => null,
+      'edit_item' => __( 'Edit Actor' ),
+      'update_item' => __( 'Update Actor' ),
+      'add_new_item' => __( 'Add New Actor' ),
+      'new_item_name' => __( 'New Actor Name' ),
+      'separate_items_with_commas' => __( 'Separate Countries with commas' ),
+      'add_or_remove_items' => __( 'Add or remove Actor' ),
+      'choose_from_most_used' => __( 'Choose from the most used Actor' ),
+      'menu_name' => __( 'Actor' ),
+    );
+
+
+    register_taxonomy('Actor','Films',array(
+      'hierarchical' => false,
+      'labels' => $labels,
+      'show_ui' => true,
+      'show_admin_column' => true,
+      'update_count_callback' => '_update_post_term_count',
+      'query_var' => true,
+      'rewrite' => array( 'slug' => 'actor' ),
+    ));
+  }
 
 function custom_post_type() {
     $labels = array(
@@ -26,6 +142,7 @@ function custom_post_type() {
             'description'         => __( 'Film news and reviews', 'united' ),
             'labels'              => $labels,
             'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
+            'taxonomies'          => array( 'Genre', 'Country', 'Year', 'Actor' ),
             'hierarchical'        => false,
             'public'              => true,
             'show_ui'             => true,
@@ -41,7 +158,10 @@ function custom_post_type() {
         );   
         register_post_type( 'Films', $args );
     }
+    add_action( 'init', 'create_genre_nonhierarchical_taxonomy', 0 );
+    add_action( 'init', 'create_Country_nonhierarchical_taxonomy', 0 );
+    add_action( 'init', 'create_Year_nonhierarchical_taxonomy', 0 );
+    add_action( 'init', 'create_Actor_nonhierarchical_taxonomy', 0 );
     add_action( 'init', 'custom_post_type', 0 );
-
 
 ?>
